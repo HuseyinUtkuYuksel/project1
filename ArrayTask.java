522cae7dc336379bd7d98ecf46891ffece5d4fc3
yagizbrotv <yagizsaridogan@gmail.com>
2023-02-08 11:24:13 +0300

    asd

--- ArrayTask.java
+++ ArrayTask.java
@@ -27,6 +27,33 @@ public class ArrayTask {
     }
     return yagiz ;
     }
+    public int minfinder(int[] minfindersarray)
+{
+    int min=101;
+    for(int i=0;i<minfindersarray.length;i++)
+    {
+        if(min>minfindersarray[i])
+        {
+            min=minfindersarray[i];
+        }
+    }
+return min;
+}
+
+
+public int maxfinder(int[] maxfindersarray)
+{
+
+int max=-1;
+    for(int i=0;i<maxfindersarray.length;i++)
+    {
+        if(max<maxfindersarray[i])
+        {
+            max=maxfindersarray[i];
+        }
+    }
+return max;
+} 
 
     public static void display(){
         Scanner in = new Scanner(System.in);
