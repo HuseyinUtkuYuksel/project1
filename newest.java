3f3ff132a1cd933afbbbc4638894629e8bcc5a1c
yagizbrotv <yagizsaridogan@gmail.com>
2023-02-08 11:11:11 +0300

    hak hll beler

---
+++ newest.java
@@ -0,0 +1,18 @@
+package project1;
+
+
+public class newest {
+
+    public static void main(String[] args) {
+        
+        int array[]={10,20,30,40};
+
+        maxminclass newMaxminclass = new maxminclass();
+       System.out.println(newMaxminclass.maxfinder(array)); 
+       System.out.println(newMaxminclass.minfinder(array)); 
+
+
+
+    }
+    
+}
